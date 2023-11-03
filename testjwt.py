1c24a9576c7082272a51f664143f397cb714839d
fdevsectest <143382993+fdevsectest@users.noreply.github.com>
2023-11-03 21:59:02 +0530

    Update testjwt.py

--- testjwt.py
+++ testjwt.py
@@ -5,7 +5,7 @@ class TestJWTDecoding(unittest.TestCase):
     def test_invalid_signature(self):
         with self.assertRaises(jwt.InvalidTokenError) as context:
             # Attempt to decode the token with a different secret key
-            jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c', key='incorrect_secret', algorithms=['HS256'])
+            jwt.decode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3NjUwIiwibmFtZSI6InNhaSIsImlhdCI6MjAyM30.2IBo2xmivgJyDB-NOsljAOaqmz2AMWdSL3Wcp6wLP8M', key='incorrect_secret', algorithms=['HS256'])
 
         self.assertEqual(str(context.exception), 'Signature verification failed')
 
